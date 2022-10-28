9ffef84f312062c2d1d953eeba4a528045355a49
olasunkanmi <olasunkanmizik@gmail.com>
2022-10-28 09:13:37 +0100

    Added jwt security to the project

---
+++ src/main/java/com/mtb/booking/route/HelloResource.java
@@ -0,0 +1,58 @@
+package com.mtb.booking.route;
+
+import com.mtb.booking.security.filters.AuthenticationRequest;
+import com.mtb.booking.security.filters.AuthenticationResponse;
+import com.mtb.booking.security.filters.JwtUtil;
+import com.mtb.booking.security.filters.MyUserDetailsService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloResource {
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private MyUserDetailsService userDetailsService;
+    @Autowired
+    private JwtUtil jwtUtil;
+    @RequestMapping("/hello")
+    public String hello(){
+        return "Hello World";
+    }
+
+    @RequestMapping("/success")
+    public String success(){
+        return "Thank you for teaching me JWT Mr. Habeeb";
+    }
+
+    @RequestMapping("/test")
+    public String test(){
+        return "Thank you for testing";
+    }
+
+    @RequestMapping(value = "/authenticate", method = RequestMethod.POST)
+        public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception{
+          try{
+
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(),authenticationRequest.getPassword())
+            );
+        }catch (BadCredentialsException e){
+                throw new Exception("Incorrect username or password", e);
+          }
+
+        final UserDetails userDetails = userDetailsService.
+                  loadUserByUsername(authenticationRequest.getUsername());
+          final String jwt = jwtUtil.generateToken(userDetails);
+          return ResponseEntity.ok(new AuthenticationResponse(jwt));
+    }
+}
---
+++ src/main/java/com/mtb/booking/security/filters/AuthenticationResponse.java
@@ -0,0 +1,13 @@
+package com.mtb.booking.security.filters;
+
+public class AuthenticationResponse {
+    private final String jwt;
+
+    public AuthenticationResponse(String jwt) {
+        this.jwt = jwt;
+    }
+
+    public String getJwt() {
+        return jwt;
+    }
+}
